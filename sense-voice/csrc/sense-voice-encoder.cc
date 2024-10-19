2900882e487ed4d276b45325fdd819e506a21ecc
lovemefan <lovemefan@outlook.com>
2024-10-20 00:12:32 +0800

    remove ggml_backend_metal_set_n_cb in metal

--- sense-voice/csrc/sense-voice-encoder.cc
+++ sense-voice/csrc/sense-voice-encoder.cc
@@ -110,8 +110,6 @@ static ggml_backend_t sense_voice_backend_init(
 #ifdef GGML_USE_METAL
     if (params.use_gpu) {
         SENSEVOICE_LOG_INFO("%s: using Metal backend\n", __func__);
-        ggml_backend_metal_log_set_callback(g_state.log_callback,
-                                            g_state.log_callback_user_data);
         backend_gpu = ggml_backend_metal_init();
         if (!backend_gpu) {
             SENSEVOICE_LOG_ERROR("%s: ggml_backend_metal_init() failed\n", __func__);
@@ -155,12 +153,6 @@ static bool ggml_graph_compute_helper(
 #ifdef GGML_USE_BLAS
         if (ggml_backend_is_blas(backend)) {
             ggml_backend_blas_set_n_threads(backend, n_threads);
-        }
-#endif
-
-#ifdef GGML_USE_METAL
-        if (ggml_backend_is_metal(backend)) {
-            ggml_backend_metal_set_n_cb(backend, n_threads);
         }
 #endif
     }
